d637c6f2f794bfdf766057ebcbee5115bc79b131
Oliver Voorhees <oliver.voorhees@stonybrook.edu>
2018-07-19 00:54:35 -0400

    Move to single page app. Fix sizing and mobile view

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,5 @@
 var slideIndex = 0;
-showSlides();
+// showSlides();
 /*
 // Next/previous controls
 function plusSlides(n) {
@@ -27,6 +27,17 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }*/
 
+function setPage(page) {
+  var i;
+  var pages = document.getElementsByClassName("myPages");
+  console.log(slideIndex);
+  for (i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none"; 
+  }
+  pages[page].style.display = "block";
+
+};
+
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -37,23 +48,23 @@ function showSlides() {
     if (slideIndex > slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
     setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
-
-function facebook(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
 };
 
+// function facebook(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v2.5";
+//   fjs.parentNode.insertBefore(js, fjs);
+// };
 
 
-$(document).ready(function(e) {
-    var width = "+=" + $(document).width();
-    $("#animate").animate({
-    left: width
-  }, 5000, function() {
-    // Animation complete.
-  });
-});
+
+// $(document).ready(function(e) {
+//     var width = "+=" + $(document).width();
+//     $("#animate").animate({
+//     left: width
+//   }, 5000, function() {
+//     // Animation complete.
+//   });
+// });
